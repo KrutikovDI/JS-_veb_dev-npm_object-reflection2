13e9fa6006e2b9a684635d375bfa557135e40272
Danil Krutikov <kdanil.i@yandex.ru>
2024-02-08 11:27:28 +0700

    Correct function

--- src/main.js
+++ src/main.js
@@ -1,18 +1,8 @@
 export default function extractionSpecialAttack(person) {
-  const listAttacks = []
-  for (let i in person.special) {
-    const listAttack = []
-    if (person.special[i].description == undefined) {
-      for (let j in person.special[i]) {
-        listAttack.push(person.special[i][j])
-        }
-        listAttack.push('Описание недоступно')
-    } else {
-      for (let j in person.special[i]) {
-        listAttack.push(person.special[i][j])
-      }
-    }
-    listAttacks.push(listAttack)
+  let attaksList = []
+  for (let index in person.special) {
+      let { id, name, icon, description = 'Описание недоступно'} = person.special[index];
+      attaksList.push([id, name, icon, description])
   }
-  return listAttacks
+  return attaksList
 }
